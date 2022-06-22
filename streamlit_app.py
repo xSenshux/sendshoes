472168906ed9faaf169930d265a03207ee00f1fa
xSenshux <107892488+xSenshux@users.noreply.github.com>
2022-06-22 15:35:16 -0700

    Update streamlit_app.py
     Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to diplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
@@ -33,3 +35,5 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+
