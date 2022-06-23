f09c820187025dd889129c0f33a5deb696f6b54b
xSenshux <107892488+xSenshux@users.noreply.github.com>
2022-06-22 19:27:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function) 
-def get_fruityvice_data(this_fruit_choice):
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+def get_fruityvice_data(fruit_choice):
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 	return fruityvice_normalized
 
@@ -39,7 +39,7 @@ try:
 	if not fruit_choice:
 		streamlit.error("Please select a fruit to get information.")
 	else:
-		back_from_function = get_fruityvice_data(this_fruit_choice)
+		back_from_function = get_fruityvice_data(fruit_choice)
 		streamlit.dataframe(back_from_function)
               
 #streamlit.header ("The fruit load list contains:")
