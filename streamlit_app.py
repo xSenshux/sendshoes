d01d90b19dd46c2c133523d7aaea95dee5f076e1
xSenshux <107892488+xSenshux@users.noreply.github.com>
2022-06-22 20:27:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+
+  
+
+
+
 #New Section to display fruitvice api response 
+streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
