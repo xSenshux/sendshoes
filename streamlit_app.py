bbe76707802d614a7594e1ffb20604075dc4ea60
xSenshux <107892488+xSenshux@users.noreply.github.com>
2022-06-22 19:33:46 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ def get_fruityvice_data(fruit_choice):
 
 
 #New section to diplay fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
 	fruit_choice = streamlit.text_input('What fruit would you like information about?')
 	if not fruit_choice:
@@ -42,22 +42,22 @@ try:
 		back_from_function = get_fruityvice_data(fruit_choice)
 		streamlit.dataframe(back_from_function)
               
-#streamlit.header ("The fruit load list contains:")
+streamlit.header ("The fruit load list contains:")
 #Snowflake-related functions
 #def get_fruit_load_list():
-#	with my_cnx.cursor() as my_cur:
-#		my_cur.execute("select * from_fruit_load_list")
-#		return my_cur.fetchall()
+	with my_cnx.cursor() as my_cur:
+		my_cur.execute("select * from_fruit_load_list")
+		return my_cur.fetchall()
 # Add a button to load the fruit
-#if streamlit.button('Get Fruit Load List'):
-#	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#	my_data _rows = get_fruit_load_list()
-#	streamlit.dataframe(my_data_rows)
+if streamlit.button('Get Fruit Load List'):
+	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+	my_data _rows = get_fruit_load_list()
+	streamlit.dataframe(my_data_rows)
    
-#add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-#streamlit.write('Thank you for adding', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.write('Thank you for adding', add_my_fruit)
 
 #don't run anything past here while we troubleshoot
-#streamlit.stop()
+streamlit.stop()
 
 #my_cur.execute("insert into fruit_load_list values ('from streamlist')")
