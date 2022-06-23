6eb7b856c885d2b9b9cd850f8bbe74bec4a10c85
xSenshux <107892488+xSenshux@users.noreply.github.com>
2022-06-22 19:14:20 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,22 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function) 
 def get_fruityvice_data(this_fruit_choice):
-   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
-   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-   return fruityvice_normalized
+	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized
 
 
 #New section to diplay fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information.")
-  else:
-      back_from_function = get_fruityvice_data(this_fruit_choice)
-      streamlit.dataframe(back_from_function)              
-                   
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+		back_from_function = get_fruityvice_data(this_fruit_choice)
+		streamlit.dataframe(back_from_function)
+		
+streamlit.stop()                  
 streamlit.header ("The fruit load list contains:")
 #Snowflake-related functions
 def get_fruit_load_list():
